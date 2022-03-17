9563f2b66be85cfbe0dec6cf6ec9a5336ae26793
ann-wangui <wanguia822@gmail.com>
2022-03-17 21:58:40 +0300

    add a README.md file

--- script.js
+++ script.js
@@ -57,7 +57,6 @@ $(function () {
             event.preventDefault();
             let name = $("input#name").val();
             let email = $("input#email").val();
-            let message = $("textarea#message").val();
 
             if ($("input#name").val() && $("input#email").val()){
                 alert ("Hey " + name + ", Thanks for reaching out, we'll get back to you shortly...Have an awesome day! ~Stay blessed");
